45564ab50d5ee795518d374e487a94491c226023
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-12-07 18:24:31 +0300

    Первая версия приложения

--- JujaRpLauncher/MainWindow.xaml.cs
+++ JujaRpLauncher/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,36 @@ namespace JujaRpLauncher
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int NumberBackground = 4;
+
         public MainWindow()
         {
             InitializeComponent();
+            MainBackgroundShow();
         }
+
+        private void MainBackgroundShow()
+        {
+            var rnd = new Random();
+            var result = rnd.Next(1, NumberBackground);
+
+            string selectedFileName = $"/Resources/background{result}.jpg";
+
+            var bitmap = new BitmapImage();
+
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(selectedFileName, UriKind.Relative);
+            bitmap.EndInit();
+
+            MainBackground.Source = bitmap;
+        }
+
+        private void ConnectToServerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", "steam://connect/185.221.196.85:27015");
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+            => Close();
     }
 }
